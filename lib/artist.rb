4448cd8a0b5b41522840b428dd7d18e38ac411df
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-10-19 00:08:14 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -34,7 +34,8 @@ class Artist
   def add_song(song)
     new_song = Song.new(song)
     #new_song.artist = self
-    new_song.name.artist = self.name
+    new_song.name = song
+    new_song.artist = self.name
     binding.pry
   end
   ###End of Instance Methods###

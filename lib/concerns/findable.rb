8794b24a0556cffc1363fd95bb68b48adf2b00f4
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-10-19 02:36:40 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,13 +1,15 @@
-module Concerns::Findable
-  def find_by_name(name)
-    self.all.find(){|song|
-      if song.name == song_name
-        song
-      end
-    }
-  end
+module Concerns
+  module Findable
+    def find_by_name(name)
+      self.all.find(){|song|
+        if song.name == song_name
+          song
+        end
+      }
+    end
 
-  def find_or_create_by_name(name)
-    self.find_by_name(name) || self.create(name)
-  end
+    def find_or_create_by_name(name)
+      self.find_by_name(name) || self.create(name)
+    end
+  end 
 end

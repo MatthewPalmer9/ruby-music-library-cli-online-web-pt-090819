14df163c784fe365529fae0f7fc08a060ce4c8fc
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-10-19 03:41:59 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -51,7 +51,22 @@ class MusicLibraryController
       }
     end
 
-    
+    def list_genres
+      Genre.all.sort{|a, b| a.name <=> b.name}.each_with_index(){|g, i|
+        puts "#{i+1}. #{g.name}"
+      }
+    end
+
+    def list_songs_by_artist
+      puts "Please enter the name of an artist:"
+      input = gets.chomp
+
+      if artist = Artist.find_by_name(input)
+        artist.songs.sort{|a, b| a.name <=> b.name}.each_with_index(){|s, i|
+          puts "#{i+1}. #{s.name} - #{s.genre.name}"
+        }
+      end
+    end
 
 
   end
